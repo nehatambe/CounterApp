d50477ba7ddd9d2314abae151876fa03b426faeb
Neha Kailas Tambe <nehatambe1191@gmail.com>
2020-06-04 11:48:55 -0700

    first commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+
+    render() {
+        return (
+            <div>
+                {this.props.counters.map(counter => (<Counter key={counter.id}
+                    onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter}>
+                    <h4>Counter #{counter.id}</h4>
+                    </Counter>))}
+                     
+            </div>
+           
+        );
+    }
+}
+
+export default Counters;
